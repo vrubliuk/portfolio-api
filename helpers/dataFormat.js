1f1b78a322df52ef7d4bc09b700d25913aa6a98f
Valentyn Rubliuk <vrubliuk@gmail.com>
2019-07-09 18:50:50 +0300

    Update the packages versions

---
+++ helpers/dataFormat.js
@@ -0,0 +1,99 @@
+const x = {
+  general: {
+    name: "Valentyn",
+    surname: "Rubliuk",
+    qualification: "Full Stack Developer",
+    avatar: avatar
+  },
+
+  contacts: {
+    location: "Lviv, UA",
+    phone: "(063) 75 74 922",
+    email: "vrubliuk@gmail.com",
+    github: "github.com/vrubliuk",
+    linkedIn: "github.com/vrubliuk"
+  },
+
+  categories: [
+    {
+      name: "Front-end",
+      technologies: ["React, Redux", "Vue, Vuex"]
+    },
+    {
+      name: "Back-end",
+      technologies: ["Node", "MongoDB"]
+    },
+    {
+      name: "Others",
+      technologies: ["Photoshop", "Gimp"]
+    }
+  ],
+
+  experiences: [
+    {
+      position: "Front-end Developer",
+      company: "Redentu",
+      city: "Lviv",
+      startDate: "2018",
+      endDate: "2019"
+    },
+    {
+      position: "Team Leader of Billing department",
+      company: "PLS Logistics Services",
+      city: "Lviv",
+      startDate: "2014",
+      endDate: "2018"
+    }
+  ],
+
+  education: {
+    speciality: "Management of foreign economic activity",
+    institution: 'NTUU "Kyiv Polytechnic Institute"',
+    startDate: "2008",
+    endDate: "2014"
+  },
+
+  languages: [
+    {
+      name: "Ukrainian",
+      level: "native"
+    },
+    {
+      name: "English",
+      level: "professional"
+    },
+    {
+      name: "JavaScript",
+      level: "fluent"
+    }
+  ],
+
+  projects: [
+    {
+      name: "Calendar",
+      screenshotURL: screenshot,
+      summary: "Some text",
+      tags: ["jQuery"],
+      websiteURL: "https://vrubliuk.github.io/painting-cart/",
+      repositoryURL: "https://github.com/vrubliuk/painting-cart/"
+    },
+    {
+      name: "Requirements",
+      screenshotURL: screenshot,
+      summary: "Some text",
+      tags: ["React", "jQuery", "Vue"],
+      websiteURL: "https://vrubliuk.github.io/painting-cart/",
+      repositoryURL: "https://github.com/vrubliuk/painting-cart/"
+    },
+    {
+      name: "Statistics",
+      screenshotURL: screenshot,
+      summary: "Some text",
+      tags: ["React", "jQuery", "Vue"],
+      websiteURL: "https://vrubliuk.github.io/painting-cart/",
+      repositoryURL: "https://github.com/vrubliuk/painting-cart/"
+    }
+  ],
+
+  resume: "file"
+};
