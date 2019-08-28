9b843a9e2b9886098d7a9f4d4024abf2db9e81a3
Valentyn Rubliuk <vrubliuk@gmail.com>
2019-08-28 17:56:57 +0300

    Add a compression package

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require("express");
-const helmet = require("helmet")
+const helmet = require("helmet");
 const mongoose = require("mongoose");
+const compression = require("compression");
 require("dotenv").config();
 const useRoutes = require("./routes");
 
 const app = express();
- app.use(helmet())
+app.use(helmet());
+app.use(compression());
 useRoutes(app);
 
 mongoose.set("useCreateIndex", true);
 mongoose
-  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@portfolio-jumvs.mongodb.net/${process.env.DB_NAME}?retryWrites=true`, { useNewUrlParser: true })
+  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@portfolio-jumvs.mongodb.net/${process.env.DB_NAME}?retryWrites=true`, {
+    useNewUrlParser: true
+  })
   .then(() => {
     app.listen(process.env.PORT || 3000);
   })
